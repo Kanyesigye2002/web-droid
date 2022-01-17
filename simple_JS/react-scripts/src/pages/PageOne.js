50c957119fd180c1c0214d7100c89db4227f8258
Dieppv98 <50536252+Dieppv98@users.noreply.github.com>
2022-01-17 11:59:21 +0700

    init project

---
+++ simple_JS/react-scripts/src/pages/PageOne.js
@@ -0,0 +1,56 @@
+// @mui
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import { useEffect, useState } from 'react';
+// components
+import Page from '../components/Page';
+// ----------------------------------------------------------------------
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+export default function PageOne() {
+  const [dataTable, setDatatable] = useState([]);
+
+  console.log(process.env.REACT_APP_API_HOST);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then((response) => response.json())
+      .then((data) => setDatatable(data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <Page title="Page One">
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Id</TableCell>
+              <TableCell align="right">Title</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {dataTable.map((row) => (
+              <TableRow key={row.id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                <TableCell component="th" scope="row">
+                  {row.id}
+                </TableCell>
+                <TableCell align="right">{row.title}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Page>
+  );
+}
