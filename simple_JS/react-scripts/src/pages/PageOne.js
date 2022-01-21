d985a9d2dad9e8965c285e53ca379d2fe32fa1e7
Dieppv98 <50536252+Dieppv98@users.noreply.github.com>
2022-01-21 23:38:35 +0700

    fixcode

--- simple_JS/react-scripts/src/pages/PageOne.js
+++ simple_JS/react-scripts/src/pages/PageOne.js
@@ -1,57 +1,299 @@
-// @mui
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import { sentenceCase } from 'change-case';
 import { useEffect, useState } from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import { useTheme } from '@mui/material/styles';
+// @mui
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Card,
+  Avatar,
+  Button,
+  Checkbox,
+  Container,
+  Typography,
+  TablePagination,
+} from '@mui/material';
+
+import { PATH_DASHBOARD } from '../routes/paths';
+// hooks
+import useSettings from '../hooks/useSettings';
+// _mock_
+import { _userList } from '../_mock';
 // components
 import Page from '../components/Page';
-// ----------------------------------------------------------------------// routes
-import { PATH_DASHBOARD } from '../routes/paths';
+import Label from '../components/Label';
+import Iconify from '../components/Iconify';
+import Scrollbar from '../components/Scrollbar';
+import SearchNotFound from '../components/SearchNotFound';
+import HeaderBreadcrumbs from '../components/HeaderBreadcrumbs';
+// sections
+import { UserListHead, UserListToolbar, UserMoreMenu } from '../sections/@dashboard/user/list';
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
+// ----------------------------------------------------------------------
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+const TABLE_HEAD = [
+  { id: 'name', label: 'Name', alignRight: false },
+  { id: 'company', label: 'Company', alignRight: false },
+  { id: 'role', label: 'Role', alignRight: false },
+  { id: 'isVerified', label: 'Verified', alignRight: false },
+  { id: 'status', label: 'Status', alignRight: false },
+  { id: '' },
 ];
 
-export default function PageOne() {
-  const [dataTable, setDatatable] = useState([]);
+// ----------------------------------------------------------------------
 
-  console.log(process.env.REACT_APP_API_HOST);
+export default function UserList() {
+  const theme = useTheme();
+  const { themeStretch } = useSettings();
 
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/todos')
-      .then((response) => response.json())
-      .then((data) => setDatatable(data))
-      .catch((err) => console.log(err));
-  }, []);
+  const [userList, setUserList] = useState(_userList);
+  const [page, setPage] = useState(0);
+  const [order, setOrder] = useState('asc');
+  const [selected, setSelected] = useState([]);
+  const [orderBy, setOrderBy] = useState('name');
+  const [filterName, setFilterName] = useState('');
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const handleRequestSort = (property) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  // const handleSelectAllClick = (checked) => {
+  //   if (checked) {
+  //     const newSelecteds = userList.map((n) => n.name);
+  //     setSelected(newSelecteds);
+  //     return;
+  //   }
+  //   setSelected([]);
+  // };
+
+  const handleClick = (name) => {
+    const selectedIndex = selected.indexOf(name);
+    let newSelected = [];
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, name);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(selected.slice(0, selectedIndex), selected.slice(selectedIndex + 1));
+    }
+    setSelected(newSelected);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const handleFilterByName = (filterName) => {
+    setFilterName(filterName);
+    setPage(0);
+  };
+
+  const handleDeleteUser = (userId) => {
+    const deleteUser = userList.filter((user) => user.id !== userId);
+    setSelected([]);
+    setUserList(deleteUser);
+  };
+
+  const handleDeleteMultiUser = (selected) => {
+    const deleteUsers = userList.filter((user) => !selected.includes(user.name));
+    setSelected([]);
+    setUserList(deleteUsers);
+  };
+
+  const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - userList.length) : 0;
+
+  const filteredUsers = applySortFilter(userList, getComparator(order, orderBy), filterName);
+
+  const isNotFound = !filteredUsers.length && Boolean(filterName);
 
   return (
-    <Page title="Page One">
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Id</TableCell>
-              <TableCell align="right">Title</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {dataTable.map((row) => (
-              <TableRow key={row.id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                <TableCell component="th" scope="row">
-                  {row.id}
-                </TableCell>
-                <TableCell align="right">{row.title}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+    <Page title="User: List">
+      <Container maxWidth={themeStretch ? false : 'lg'}>
+        <HeaderBreadcrumbs
+          heading="User List"
+          links={[
+            { name: 'Dashboard', href: PATH_DASHBOARD.root },
+            { name: 'User', href: PATH_DASHBOARD.user.root },
+            { name: 'List' },
+          ]}
+          action={
+            <Button
+              variant="contained"
+              component={RouterLink}
+              to={PATH_DASHBOARD.user.newUser}
+              startIcon={<Iconify icon={'eva:plus-fill'} />}
+            >
+              New User
+            </Button>
+          }
+        />
+
+        <Card>
+          <UserListToolbar
+            filterName={filterName}
+            onFilterName={handleFilterByName}
+            onDeleteUsers={() => handleDeleteMultiUser(selected)}
+          />
+
+          <Scrollbar>
+            <TableContainer sx={{ minWidth: 800 }}>
+              <Table>
+                <UserListHead
+                  order={order}
+                  orderBy={orderBy}
+                  headLabel={TABLE_HEAD}
+                  rowCount={userList.length}
+                  numSelected={selected.length}
+                  onRequestSort={handleRequestSort}
+                />
+                <TableBody>
+                  {filteredUsers.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
+                    const { id, name, role, status, company, isVerified } = row;
+
+                    return (
+                      <TableRow hover key={id} tabIndex={-1}>
+                        <TableCell align="center">{name}</TableCell>
+                        <TableCell align="center">{company}</TableCell>
+                        <TableCell align="center">{role}</TableCell>
+                        <TableCell align="center">{isVerified ? 'Yes' : 'No'}</TableCell>
+                        <TableCell align="center">
+                          <Label
+                            variant={theme.palette.mode === 'light' ? 'ghost' : 'filled'}
+                            color={(status === 'banned' && 'error') || 'success'}
+                          >
+                            {sentenceCase(status)}
+                          </Label>
+                        </TableCell>
+
+                        <TableCell align="center">
+                          <UserMoreMenu onDelete={() => handleDeleteUser(id)} userName={name} />
+                        </TableCell>
+                      </TableRow>
+                    );
+                  })}
+                  {emptyRows > 0 && (
+                    <TableRow style={{ height: 53 * emptyRows }}>
+                      <TableCell colSpan={6} />
+                    </TableRow>
+                  )}
+                </TableBody>
+                {isNotFound && (
+                  <TableBody>
+                    <TableRow>
+                      <TableCell align="center" colSpan={6} sx={{ py: 3 }}>
+                        <SearchNotFound searchQuery={filterName} />
+                      </TableCell>
+                    </TableRow>
+                  </TableBody>
+                )}
+              </Table>
+            </TableContainer>
+          </Scrollbar>
+
+          <TablePagination
+            rowsPerPageOptions={[5, 10, 25]}
+            component="div"
+            count={userList.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={(e, page) => setPage(page)}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
+        </Card>
+      </Container>
     </Page>
   );
 }
+
+// ----------------------------------------------------------------------
+
+function descendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function getComparator(order, orderBy) {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function applySortFilter(array, comparator, query) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  if (query) {
+    return array.filter((_user) => _user.name.toLowerCase().indexOf(query.toLowerCase()) !== -1);
+  }
+  return stabilizedThis.map((el) => el[0]);
+}
+
+// function createData(name, calories, fat, carbs, protein) {
+//   return { name, calories, fat, carbs, protein };
+// }
+
+// const rows = [
+//   createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+//   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+//   createData('Eclair', 262, 16.0, 24, 6.0),
+//   createData('Cupcake', 305, 3.7, 67, 4.3),
+//   createData('Gingerbread', 356, 16.0, 49, 3.9),
+// ];
+
+// export default function PageOne() {
+//   const [dataTable, setDatatable] = useState([]);
+
+//   console.log(process.env.REACT_APP_API_HOST);
+
+//   useEffect(() => {
+//     fetch('https://jsonplaceholder.typicode.com/todos')
+//       .then((response) => response.json())
+//       .then((data) => setDatatable(data))
+//       .catch((err) => console.log(err));
+//   }, []);
+
+//   return (
+//     <Page title="Page One">
+//       <TableContainer component={Paper}>
+//         <Table sx={{ minWidth: 650 }} aria-label="simple table">
+//           <TableHead>
+//             <TableRow>
+//               <TableCell>Id</TableCell>
+//               <TableCell align="right">Title</TableCell>
+//             </TableRow>
+//           </TableHead>
+//           <TableBody>
+//             {dataTable.map((row) => (
+//               <TableRow key={row.id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+//                 <TableCell component="th" scope="row">
+//                   {row.id}
+//                 </TableCell>
+//                 <TableCell align="right">{row.title}</TableCell>
+//               </TableRow>
+//             ))}
+//           </TableBody>
+//         </Table>
+//       </TableContainer>
+//     </Page>
+//   );
+// }
