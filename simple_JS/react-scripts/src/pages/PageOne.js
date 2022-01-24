9e480ce309119d36a3615aea4b52b79a6330d789
Dieppv98 <50536252+Dieppv98@users.noreply.github.com>
2022-01-24 14:39:07 +0700

    fixcode

--- simple_JS/react-scripts/src/pages/PageOne.js
+++ simple_JS/react-scripts/src/pages/PageOne.js
@@ -24,7 +24,7 @@ import { PATH_DASHBOARD } from '../routes/paths';
 // hooks
 import useSettings from '../hooks/useSettings';
 // _mock_
-import { _userList } from '../_mock';
+// import { _userList } from '../_mock';
 // components
 import Page from '../components/Page';
 import Label from '../components/Label';
@@ -38,25 +38,40 @@ import { UserListHead, UserListToolbar, UserMoreMenu } from '../sections/@dashbo
 // ----------------------------------------------------------------------
 
 const TABLE_HEAD = [
-  { id: 'name', label: 'Name', alignRight: false },
-  { id: 'company', label: 'Company', alignRight: false },
-  { id: 'role', label: 'Role', alignRight: false },
-  { id: 'isVerified', label: 'Verified', alignRight: false },
-  { id: 'status', label: 'Status', alignRight: false },
+  { id: 'ten_san_pham', label: 'Tên sản phẩm', alignRight: false },
+  { id: 'ma_san_pham', label: 'Mã sản phẩm', alignRight: false },
+  { id: 'total_quantity', label: 'Tổng nhập', alignRight: false },
+  { id: 'quantity_current', label: 'Số lượng hiện tại', alignRight: false },
+  { id: 'unit_name', label: 'Đơn vị tính', alignRight: false },
   { id: '' },
 ];
 
 // ----------------------------------------------------------------------
+// console.log(process.env.REACT_APP_API_HOST);
 
 export default function UserList() {
   const theme = useTheme();
   const { themeStretch } = useSettings();
 
-  const [userList, setUserList] = useState(_userList);
+  const [userList, setUserList] = useState([]);
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ Keywords: '' }),
+  };
+
+  useEffect(() => {
+    fetch(`http://171.244.141.179:7000/product/getall`, requestOptions)
+      .then((response) => response.json())
+      .then((data) => setUserList(data.data))
+      .catch((err) => console.log(err));
+  });
+  console.log(userList);
+
   const [page, setPage] = useState(0);
   const [order, setOrder] = useState('asc');
   const [selected, setSelected] = useState([]);
-  const [orderBy, setOrderBy] = useState('name');
+  const [orderBy, setOrderBy] = useState('ten_san_pham');
   const [filterName, setFilterName] = useState('');
   const [rowsPerPage, setRowsPerPage] = useState(5);
 
@@ -66,29 +81,20 @@ export default function UserList() {
     setOrderBy(property);
   };
 
-  // const handleSelectAllClick = (checked) => {
-  //   if (checked) {
-  //     const newSelecteds = userList.map((n) => n.name);
-  //     setSelected(newSelecteds);
-  //     return;
+  // const handleClick = (name) => {
+  //   const selectedIndex = selected.indexOf(name);
+  //   let newSelected = [];
+  //   if (selectedIndex === -1) {
+  //     newSelected = newSelected.concat(selected, name);
+  //   } else if (selectedIndex === 0) {
+  //     newSelected = newSelected.concat(selected.slice(1));
+  //   } else if (selectedIndex === selected.length - 1) {
+  //     newSelected = newSelected.concat(selected.slice(0, -1));
+  //   } else if (selectedIndex > 0) {
+  //     newSelected = newSelected.concat(selected.slice(0, selectedIndex), selected.slice(selectedIndex + 1));
   //   }
-  //   setSelected([]);
+  //   setSelected(newSelected);
   // };
-
-  const handleClick = (name) => {
-    const selectedIndex = selected.indexOf(name);
-    let newSelected = [];
-    if (selectedIndex === -1) {
-      newSelected = newSelected.concat(selected, name);
-    } else if (selectedIndex === 0) {
-      newSelected = newSelected.concat(selected.slice(1));
-    } else if (selectedIndex === selected.length - 1) {
-      newSelected = newSelected.concat(selected.slice(0, -1));
-    } else if (selectedIndex > 0) {
-      newSelected = newSelected.concat(selected.slice(0, selectedIndex), selected.slice(selectedIndex + 1));
-    }
-    setSelected(newSelected);
-  };
 
   const handleChangeRowsPerPage = (event) => {
     setRowsPerPage(parseInt(event.target.value, 10));
@@ -107,7 +113,7 @@ export default function UserList() {
   };
 
   const handleDeleteMultiUser = (selected) => {
-    const deleteUsers = userList.filter((user) => !selected.includes(user.name));
+    const deleteUsers = userList.filter((user) => !selected.includes(user.ten_san_pham));
     setSelected([]);
     setUserList(deleteUsers);
   };
@@ -122,12 +128,8 @@ export default function UserList() {
     <Page title="User: List">
       <Container maxWidth={themeStretch ? false : 'lg'}>
         <HeaderBreadcrumbs
-          heading="User List"
-          links={[
-            { name: 'Dashboard', href: PATH_DASHBOARD.root },
-            { name: 'User', href: PATH_DASHBOARD.user.root },
-            { name: 'List' },
-          ]}
+          heading="Danh sách sản phẩm"
+          links={[{ name: '', href: '' }]}
           action={
             <Button
               variant="contained"
@@ -160,25 +162,18 @@ export default function UserList() {
                 />
                 <TableBody>
                   {filteredUsers.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
-                    const { id, name, role, status, company, isVerified } = row;
+                    const { id, ten_san_pham, ma_san_pham, total_quantity, quantity_current, unit_name } = row;
 
                     return (
                       <TableRow hover key={id} tabIndex={-1}>
-                        <TableCell align="center">{name}</TableCell>
-                        <TableCell align="center">{company}</TableCell>
-                        <TableCell align="center">{role}</TableCell>
-                        <TableCell align="center">{isVerified ? 'Yes' : 'No'}</TableCell>
-                        <TableCell align="center">
-                          <Label
-                            variant={theme.palette.mode === 'light' ? 'ghost' : 'filled'}
-                            color={(status === 'banned' && 'error') || 'success'}
-                          >
-                            {sentenceCase(status)}
-                          </Label>
-                        </TableCell>
+                        <TableCell align="center">{ten_san_pham}</TableCell>
+                        <TableCell align="center">{ma_san_pham}</TableCell>
+                        <TableCell align="center">{total_quantity}</TableCell>
+                        <TableCell align="center">{quantity_current}</TableCell>
+                        <TableCell align="center">{unit_name}</TableCell>
 
                         <TableCell align="center">
-                          <UserMoreMenu onDelete={() => handleDeleteUser(id)} userName={name} />
+                          <UserMoreMenu onDelete={() => handleDeleteUser(id)} userName={ten_san_pham} />
                         </TableCell>
                       </TableRow>
                     );
@@ -243,7 +238,11 @@ function applySortFilter(array, comparator, query) {
     return a[1] - b[1];
   });
   if (query) {
-    return array.filter((_user) => _user.name.toLowerCase().indexOf(query.toLowerCase()) !== -1);
+    return array.filter(
+      (_user) =>
+        _user.ten_san_pham.toLowerCase().indexOf(query.toLowerCase()) !== -1 ||
+        _user.ma_san_pham.toLowerCase().indexOf(query.toLowerCase()) !== -1
+    );
   }
   return stabilizedThis.map((el) => el[0]);
 }
@@ -262,8 +261,6 @@ function applySortFilter(array, comparator, query) {
 
 // export default function PageOne() {
 //   const [dataTable, setDatatable] = useState([]);
-
-//   console.log(process.env.REACT_APP_API_HOST);
 
 //   useEffect(() => {
 //     fetch('https://jsonplaceholder.typicode.com/todos')
